be3a779f7ed0d5eb4c0dfb695a1cc6985853141a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-16 18:26:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+    let thankYou = []
+    for (let i = 0; i < namesArray.length; i++){
+        thankYou.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    } 
+    console.log(number)
+}
